135c6a07069b9389c53f1e4c2193a6b7ba7d24d0
RachanaReddy66 <116415047+RachanaReddy66@users.noreply.github.com>
2024-02-26 11:48:27 +0530

    Revert "String"

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,9 @@
 class MainActivity extends AppcompactActivity{
-String s;
+
   @override
   private void onCreate(Bundle savedInstances){
 
     System.out.println("In onCreate")
-      
   }
   
 
