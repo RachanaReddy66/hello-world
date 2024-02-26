55530508aaa9b3fa40e0c7e530e801f5c8ec5f12
RachanaReddy66 <116415047+RachanaReddy66@users.noreply.github.com>
2024-02-26 11:50:20 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,7 +3,9 @@ class MainActivity extends AppcompactActivity{
   @override
   private void onCreate(Bundle savedInstances){
 
-    System.out.println("In onCreate")
+    System.out.println("In onCreate");
+        System.out.println("In onCreate1")
+
   }
   
 
